22478a2fcc8a18163225a5353c260ae83694b403
Oblamov <ahmet_nt@outlook.com>
2024-03-24 23:43:44 +0300

    php up to date

--- localconnect/register.php
+++ localconnect/register.php
@@ -1,11 +1,12 @@
 <?php
 $servername = "localhost";
 $username = "root";
+$email = "";
 $password = "";
 $database = "localconnect";
-$email = "";
+
 // Create connection
-$conn = new mysqli($servername, $username, $password,$email,$database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
@@ -17,11 +18,17 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 
-$sql = "INSERT INTO users(username,password,email) VALUES('".$username."','".$password."','".$email."')";
+$sql = "INSERT INTO users (username, email, password)
+    SELECT * FROM (SELECT '".$username."' AS username, '".$email."' AS email,'".$password."' as password) AS tmp
+    WHERE NOT EXISTS (
+        SELECT 1 FROM users WHERE email = '".$email."')";
+
 $query = mysqli_query($conn, $sql);
 
 if($query){
     echo json_encode("Register Successed");
+}else {
+    echo json_encode("This email already exist");
 }
 
 ?>
