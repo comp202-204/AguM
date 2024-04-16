b33158aed348bcdcb2778d84c81dad64f4e596f4
Mehmet Koray Özdemir <mehmetkorayozdemir@Mehmet-MacBook-Pro-2.local>
2024-04-16 21:08:56 +0300

    koray

--- lib/pages/DetailPages/CampusMapDetailPage.dart
+++ lib/pages/DetailPages/CampusMapDetailPage.dart
@@ -8,76 +8,81 @@ class CampusMapDetailPage extends StatelessWidget {
         title: Text('Campus Map Detail'),
       ),
       body: ListViewCampus(),
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
     );
   }
 }
 
 class ListViewCampus extends StatelessWidget {
-  @override
-  final List<DecoratedBox> boxes = [
-
-    const DecoratedBox(             //Lab Boxes
+  final List<Container> boxes = [
+    Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/photos/lab1.jpeg'), // Use an asset image
-          fit: BoxFit.cover, // Scale the image to cover the entire box
+          image: AssetImage('assets/photos/lab1.jpeg'),
+          fit: BoxFit.cover,
         ),
       ),
       child: Center(
         child: Text(
-          'Lab', // Kutunun içindeki metin
+          'Lab',
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     ),
-
-    const DecoratedBox(             //SteelBuilding
+    Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/photos/steelBuilt.jpg'), // Use an asset image
-          fit: BoxFit.cover, // Scale the image to cover the entire box
+          image: AssetImage('assets/photos/steelBuilt.jpg'),
+          fit: BoxFit.cover,
         ),
       ),
       child: Center(
         child: Text(
-          'SteelBuilding', // Kutunun içindeki metin
+          'Steel Building',
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     ),
-
-    const DecoratedBox(             //Ambar
+    Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/photos/Ambar.jpg'), // Use an asset image
-          fit: BoxFit.cover, // Scale the image to cover the entire box
+          image: AssetImage('assets/photos/Ambar.jpg'),
+          fit: BoxFit.cover,
         ),
       ),
       child: Center(
         child: Text(
-          'Ambar', // Kutunun içindeki metin
+          'Ambar',
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     ),
-
-    const DecoratedBox(             //Factory
+    Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/photos/factorY.jpg'), // Use an asset image
-          fit: BoxFit.cover, // Scale the image to cover the entire box
+          image: AssetImage('assets/photos/factorY.jpg'),
+          fit: BoxFit.cover,
         ),
       ),
       child: Center(
         child: Text(
-          'Factory', // Kutunun içindeki metin
+          'Factory',
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     ),
   ];
 
+  final List<List<String>> boxItems = [
+    ['Lab Item 1', 'Lab Item 2', 'Lab Item 3'],
+    ['Steel Building Item 1', 'Steel Building Item 2'],
+    ['Ambar Item 1', 'Ambar Item 2', 'Ambar Item 3', 'Ambar Item 4'],
+    ['Factory Item 1', 'Factory Item 2', 'Factory Item 3'],
+  ];
+
+  final List<String> listItemTitles = ['Lab', 'Steel Building', 'Ambar', 'Factory'];
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: boxes.length,
@@ -87,19 +92,23 @@ class ListViewCampus extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => DetailPage(index: index),
+                builder: (context) => DetailBoxView(
+                  title: 'Details for ${listItemTitles[index]}',
+                  boxDecoration: boxes[index].decoration as BoxDecoration,
+                  items: boxItems[index],
+                ),
               ),
             );
           },
           child: Padding(
             padding: EdgeInsets.all(8.0),
-            child: ClipRRect (
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(25),
               child: SizedBox(
                 width: 200,
                 height: 150,
                 child: boxes[index],
-             ),
+              ),
             ),
           ),
         );
@@ -108,23 +117,78 @@ class ListViewCampus extends StatelessWidget {
   }
 }
 
-class DetailPage extends StatelessWidget {
-  final int index;
+class DetailBoxView extends StatelessWidget {
+  final String title;
+  final BoxDecoration boxDecoration;
+  final List<String> items;
 
-  DetailPage({required this.index});
+  DetailBoxView({
+    required this.title,
+    required this.boxDecoration,
+    required this.items,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Box ${index+1} Detail'),
+        title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Detay sayfası',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        decoration: boxDecoration,
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailItemView(
+                      title: 'Item Detail', // Pass a title for the detail page
+                      item: items[index],
+                    ),
+                  ),
+                );
+              },
+              child: ListTile(
+                title: Text(items[index]),
+              ),
+            );
+          },
         ),
       ),
     );
   }
+}
+
+class DetailItemView extends StatelessWidget {
+  final String title;
+  final String item;
+
+  DetailItemView({required this.title, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(item),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Campus Map Detail'),
+      ),
+      body: ListViewCampus(),
+      backgroundColor: Colors.grey,
+    ),
+  ));
 }
